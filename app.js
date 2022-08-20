5d54844d216f930e6562a5626c6427e0d9f94cca
kpsingh <pkrishnapal08@gmail.com>
2022-08-20 19:44:30 +0530

    Level 1 completed,by just storing email and password.

--- app.js
+++ app.js
@@ -11,7 +11,79 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/wikiDB");
+mongoose.connect('mongodb://localhost:27017/userDB');
+
+const userSchema = {
+    email : String,
+    password : String
+}
+
+const User = new mongoose.model("User", userSchema);
+
+app.get("/", function(req, res){
+    res.render("home");
+});
+
+app.route("/login")
+.get( function(req, res){
+    res.render("login");
+})
+.post(function(req, res){
+    const username= req.body.username;
+    const password = req.body.password;
+
+    User.findOne({email : username}, function(err, found){
+        if(err){
+            console.log(err);
+        }else{
+            if(found){
+                if(found.password === password){
+                    res.render("secrets");
+                }else{
+                    res.send("Password is incorrect.");
+                }
+            }else{
+                res.send("User not found.");
+            }
+        }
+    })
+});
+
+app.route("/register")
+.get( function(req, res){
+    res.render("register");
+})
+.post(function(req, res){
+    const user = new User({
+        email : req.body.username,
+        password : req.body.password
+    });
+
+    user.save(function(err){
+        if(err){
+            console.log(err);
+        }else{
+            res.render("secrets");
+        }
+    })
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 app.listen(3000, function(){
     console.log("server started on PORT 3000");
