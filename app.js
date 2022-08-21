9f5260ee3706c9f7a953400a7f8ed36966cb4f1a
kpsingh <pkrishnapal08@gmail.com>
2022-08-21 12:29:15 +0530

    Authenticate using passport and sessions

--- app.js
+++ app.js
@@ -3,15 +3,26 @@ const express = require("express");
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const ejs = require("ejs");
-const encrypt = require('mongoose-encryption');
-const bcrypt = require('bcrypt');
-const saltRounds = 10;
+const session = require('express-session');
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
 
 const app = express();
-app.set('view engine', 'ejs');
 
+app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+
+// app.set('trust proxy', 1) // trust first proxy
+app.use(session({
+  secret: 'This is my secret key',
+  resave: false,
+  saveUninitialized: false,
+  // cookie: { secure: true }
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 mongoose.connect('mongodb://localhost:27017/userDB');
 
@@ -20,40 +31,28 @@ var userSchema = new mongoose.Schema({
     password: String
 });
 
+userSchema.plugin(passportLocalMongoose);
+
 const User = new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());           // serealize and deserialize only for express sessions for cookies.
+passport.deserializeUser(User.deserializeUser());
+
 
 app.get("/", function(req, res){
     res.render("home");
 });
 
-app.route("/login")
-.get( function(req, res){
-    res.render("login");
+app.get("/logout", function(req, res){
+    req.logout(function(err) {
+      if (err) {
+          console.log(err);
+      }else
+            res.redirect('/');
+    });
 })
-.post(function(req, res){
 
-    const username= req.body.username;
-    const password = (req.body.password);
-
-    User.findOne({email : username}, function(err, found){
-        if(err){
-            console.log(err);
-        }else{
-            if(found){
-                bcrypt.compare(password, found.password, function(err, result) {
-                    // result == true
-                    if(result === true){
-                        res.render("secrets");
-                    }else{
-                        res.send("Password is incorrect.");
-                    }
-                });
-            }else{
-                res.send("User not found.");
-            }
-        }
-    })
-});
 
 app.route("/register")
 .get( function(req, res){
@@ -61,23 +60,51 @@ app.route("/register")
 })
 .post(function(req, res){
 
-    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-        const user = new User({
-            email : req.body.username,
-            password : hash
-        });
+    User.register({username:req.body.username} , req.body.password, function(err, user) {
+        if (err) {
+            console.log(err);
+            res.redirect("/register");
+        }else{
+            // const authenticate = User.authenticate();
+            passport.authenticate("local")(req, res, function() {
+                res.redirect("/secrets");
+            })
+        };
+    });
+});
 
-        user.save(function(err){
-            if(err){
-                console.log(err);
-            }else{
-                res.render("secrets");
-            }
-        })
+app.get("/secrets", function(req, res){
+    if(req.isAuthenticated()){
+        res.render("secrets");
+    }else{
+        res.redirect("/login");
+    }
+})
+
+app.route("/login")
+.get( function(req, res){
+    res.render("login");
+})
+.post(function(req, res){
+    const user = new User({
+        username : req.body.username,
+        password : req.body.password
     });
 
+    req.login(user, function(err){
+        if(err){
+            console.log(err);
+            res.redirect("/login");
+        }else{
+            passport.authenticate("local")(req, res, function(){
+                res.redirect("/secrets");
+            });
+        };
+    })
 
 });
+
+
 
 
 
